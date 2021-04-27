e5dbf4047c53ccb5d3959dd69cff50fee97c05e4
Mihil Jose <>
2021-04-27 05:22:55 +0000

    Fixed an error

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def is_leap(year):
 
 def days_in_month(year,month):
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
-  if is_leap(year):
+  if is_leap(year) and month ==2:
     return 29
   else:
     return month_days[month-1] 
